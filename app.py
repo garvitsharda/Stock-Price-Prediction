0b63c77ccf4cc248f45efd927143bf89d7bb1e57
garvit <garvitsharda07@gmail.com>
2024-09-01 18:21:18 +0530

    addition

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model = load_model(r"C:\Users\garvit\Desktop\Stock-Price-Prediction\Stock Predictions Model.keras")
+model = load_model("Stock Predictions Model.keras")
 
 st.header('Stock Market Predictor')
 
